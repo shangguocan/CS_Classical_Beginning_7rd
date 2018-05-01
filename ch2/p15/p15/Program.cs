fef9c8d939bfca1635c7e07134bb4ac15b8dafeb
shangguocan <shangguocan@gmail.com>
2018-05-01 23:23:10 +0800

    commit

---
+++ ch2/p15/p15/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace p15
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// Output text to the screen
+			Console.WriteLine ("The first app in Beginning Visual C# 2015!");
+			Console.ReadKey ();
+		}
+	}
+}
